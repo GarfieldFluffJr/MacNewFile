0015e31dd7c5181312744fc1b9635aee92af814d
Louie <louieyin6@gmail.com>
2026-01-25 16:38:59 -0500

    First commit - Finder Extension + Objective C template

---
+++ MacNewFileFinderExtension/FinderSync.h
@@ -0,0 +1,13 @@
+//
+//  FinderSync.h
+//  MacNewFileFinderExtension
+//
+//  Created by Louie Yin on 2026-01-25.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <FinderSync/FinderSync.h>
+
+@interface FinderSync : FIFinderSync
+
+@end
